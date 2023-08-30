27ccba454412856bdef8b76ef965377d60a4e86b
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-08-31 01:14:37 +0400

    init contracts

---
+++ src/TokenFund.sol
@@ -0,0 +1,153 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin/token/ERC20/ERC20.sol";
+import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import "./IUniswapV2Router01.sol";
+import "forge-std/console.sol";
+
+contract TokenFund {
+    using SafeERC20 for ERC20;
+
+    ERC20 public immutable usdc;
+    ERC20 public immutable usdt;
+    ERC20 public immutable weth;
+    ERC20 public immutable link;
+
+    IUniswapV2Router01 public immutable uniswapV2Router;
+    IUniswapV2Router01 public immutable sushiswapRouter;
+
+    constructor(
+        address _usdc,
+        address _usdt,
+        address _weth,
+        address _link,
+        address _uniswapV2Router,
+        address _sushiswapRouter
+    ) {
+        usdc = ERC20(_usdc);
+        usdt = ERC20(_usdt);
+        weth = ERC20(_weth);
+        link = ERC20(_link);
+        uniswapV2Router = IUniswapV2Router01(_uniswapV2Router);
+        sushiswapRouter = IUniswapV2Router01(_sushiswapRouter);
+    }
+
+    function depositUSDC(uint256 amount) external {
+        usdc.safeTransferFrom(msg.sender, address(this), amount);
+
+        uint halfAmount = amount / 2;
+
+        (
+            IUniswapV2Router01 dexForWeth,
+            IUniswapV2Router01 dexForLink,
+            uint256 wethOut,
+            uint256 linkOut,
+            address[] memory wethPath,
+            address[] memory linkPath
+        ) = _getDexPrices(address(usdc), halfAmount);
+
+        _swapTokens(
+            usdc,
+            dexForWeth,
+            dexForLink,
+            halfAmount,
+            wethPath,
+            linkPath
+        );
+
+        console.log("WETH RECEIVED: %s", wethOut);
+        console.log("LINK RECEIVED: %s", linkOut);
+    }
+
+    /// @param _stableCoin USDT or USDC
+    function _getDexPrices(
+        address _stableCoin,
+        uint256 _amountIn
+    )
+        internal
+        view
+        returns (
+            IUniswapV2Router01 dexForWeth,
+            IUniswapV2Router01 dexForLink,
+            uint256 wethOut,
+            uint256 linkOut,
+            address[] memory wethPath,
+            address[] memory linkPath
+        )
+    {
+        wethPath = new address[](2);
+        wethPath[0] = address(_stableCoin);
+        wethPath[1] = address(weth);
+
+        uint256 uniswapWethOut = uniswapV2Router.getAmountsOut(
+            _amountIn,
+            wethPath
+        )[1];
+        uint256 sushiswapWethOut = sushiswapRouter.getAmountsOut(
+            _amountIn,
+            wethPath
+        )[1];
+
+        linkPath = new address[](2);
+        linkPath[0] = address(_stableCoin);
+        linkPath[1] = address(link);
+
+        uint256 uniswapLinkOut = uniswapV2Router.getAmountsOut(
+            _amountIn,
+            linkPath
+        )[1];
+        uint256 sushiswapLinkOut = sushiswapRouter.getAmountsOut(
+            _amountIn,
+            linkPath
+        )[1];
+
+        console.log("UNISWAP WETH OUT: %s", uniswapWethOut);
+        console.log("SUSHISWAP WETH OUT: %s", sushiswapWethOut);
+        console.log("UNISWAP LINK OUT: %s", uniswapLinkOut);
+        console.log("SUSHISWAP LINK OUT: %s", sushiswapLinkOut);
+
+        if (uniswapWethOut > sushiswapWethOut) {
+            wethOut = uniswapWethOut;
+            dexForWeth = uniswapV2Router;
+        } else {
+            wethOut = sushiswapWethOut;
+            dexForWeth = sushiswapRouter;
+        }
+
+        if (uniswapLinkOut > sushiswapLinkOut) {
+            linkOut = uniswapLinkOut;
+            dexForLink = uniswapV2Router;
+        } else {
+            linkOut = sushiswapLinkOut;
+            dexForLink = sushiswapRouter;
+        }
+    }
+
+    function _swapTokens(
+        ERC20 _stableCoin,
+        IUniswapV2Router01 dexForWeth,
+        IUniswapV2Router01 dexForLink,
+        uint256 amount,
+        address[] memory wethPath,
+        address[] memory linkPath
+    ) internal {
+        _stableCoin.approve(address(dexForWeth), amount);
+        dexForWeth.swapExactTokensForTokens(
+            amount,
+            0,
+            wethPath,
+            address(this),
+            block.timestamp
+        );
+
+        _stableCoin.approve(address(dexForLink), amount);
+        dexForLink.swapExactTokensForTokens(
+            amount,
+            0,
+            linkPath,
+            address(this),
+            block.timestamp
+        );
+    }
+}
---
+++ test/TokenFund.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/TokenFund.sol";
+import "openzeppelin/token/ERC20/IERC20.sol";
+
+contract TokenFundTest is Test {
+    TokenFund public tokenFund;
+
+    address public constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant LINK = 0x514910771AF9Ca656af840dff83E8264EcF986CA;
+
+    address public constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    address public constant SUSHISWAP_ROUTER =
+        0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
+
+    function setUp() public {
+        tokenFund = new TokenFund(
+            USDC,
+            USDT,
+            WETH,
+            LINK,
+            UNISWAP_V2_ROUTER,
+            SUSHISWAP_ROUTER
+        );
+    }
+
+    function testDepositUSDC() public {
+        uint amount = 1000000000; /// @dev 1000 $USDC
+        deal(USDC, address(this), amount);
+        IERC20(USDC).approve(address(tokenFund), amount);
+        tokenFund.depositUSDC(amount);
+    }
+}
