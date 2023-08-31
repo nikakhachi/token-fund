490fe52c18c8d3e377de1498cc305df680c9824a
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-08-31 17:54:21 +0400

    update contracts pragma to ^0.8.20

--- src/DexOperations.sol
+++ src/DexOperations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import "openzeppelin/token/ERC20/ERC20.sol";
 import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
